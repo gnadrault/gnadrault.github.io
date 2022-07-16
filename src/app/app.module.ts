7f8963a137eef878c8c152dbaa919ed40e6bcc8a
gnadrault <gnadro@gmail.com>
2022-07-16 18:05:33 +0200

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { PopupComponent } from './core/popup/popup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    HomeComponent,
+    ProjectsComponent,
+    HeaderComponent,
+    FooterComponent,
+    PopupComponent
   ],
   imports: [
     BrowserModule,
