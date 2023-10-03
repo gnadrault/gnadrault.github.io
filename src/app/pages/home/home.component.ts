ebd80e7a55d4a765a7be9acda9f4824d48f5238b
Guillaume Nadrault <gnadro@gmail.com>
2023-10-03 21:28:57 +0200

    Update CV

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  readonly url = 'assets/download/CV_Guillaume_NADRAULT.pdf';
-  readonly filename = 'CV_Guillaume_NADRAULT.pdf';
+  readonly url = 'assets/download/CV_Guillaume_Nadrault.pdf';
+  readonly filename = 'CV_Guillaume_Nadrault.pdf';
 
   constructor() {}
 
